b2af2c647faa01914d4f134e46e42d76160ed5ca
rowaxl <rowaxl0@gmail.com>
2021-01-09 11:23:45 +0900

    clone from origin

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const redditData = require('./data.json');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname,'/views'));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'/views-hbs'));
+
+app.get('/', (req, res) => {
+    //beginning of---for handlebars sample data
+    const data = redditData['chickens'];
+    //end of----for handlebars sample data
+    res.render('home', {
+        title: 'HOME PAGE',
+        ...data
+    });
+});
+
+app.get('/cats', (req, res) => {
+    const cats = ['Blue', 'Winston', 'Ning']
+    res.render('cats', { cats });
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params
+    // console.log(subreddit);
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit', { ...data });
+    }else{
+        res.render('notfound', { subreddit });
+    }
+});
+
+app.listen(5000, () => console.log('Server is running on PORT: 5000'))
